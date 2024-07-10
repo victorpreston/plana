85d476ef4fcee0a70bd0f9fd93825e50d851c3d2
victorpreston <prestonvictor25@gmail.com>
2024-07-10 23:21:57 +0300

    feat(frontend): validation of forms, add events page with filter functionalities.

--- frontend/src/app/components/events/events.component.ts
+++ frontend/src/app/components/events/events.component.ts
@@ -1,15 +1,56 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormBuilder } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { NavbarComponent } from "../navbar/navbar.component";
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-events',
   standalone: true,
-  imports: [RouterLink, CommonModule, NavbarComponent],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink, NavbarComponent, FooterComponent],
   templateUrl: './events.component.html',
-  styleUrl: './events.component.css'
+  styleUrls: ['./events.component.css']
 })
 export class EventsComponent {
-  
+  filterForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.filterForm = this.fb.group({
+      location: [''],
+      category: [''],
+      time: this.fb.group({
+        anytime: [false],
+        thisWeek: [false],
+        nextWeek: [false],
+        thisMonth: [false],
+        nextMonth: [false],
+        thisYear: [false]
+      }),
+      price: [0],
+      search: ['']
+    });
+  }
+
+  onClearFilters() {
+    this.filterForm.reset({
+      location: '',
+      category: '',
+      time: {
+        anytime: false,
+        thisWeek: false,
+        nextWeek: false,
+        thisMonth: false,
+        nextMonth: false,
+        thisYear: false
+      },
+      price: 0,
+      search: ''
+    });
+  }
+
+  onSearch() {
+    const filters = this.filterForm.value;
+    console.log(filters);
+  }
 }
